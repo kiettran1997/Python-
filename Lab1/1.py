262189477f148170af7f06adbf6292d185e5a0e7
kiettran1997 <70044100+kiettran1997@users.noreply.github.com>
2021-09-14 18:53:44 +0700

    update new  1

--- Lab1/1.py
+++ Lab1/1.py
@@ -1,12 +1,1 @@
 # bài 3  
-def sumDigit(num):
-    sum = 0
-    while(num):
-        sum += num % 10
-        num = int(num / 10)
-    return sum
-
-print('Ket qua nho nhat la:', min(100, 321, 267, 59, 40, key=sumDigit))
-
-num = [15, 300, 2700, 821, 52, 10, 6]
-print('Ket qua nho nhat la:', min(num, key=sumDigit))
