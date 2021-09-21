60441b53f46991129cdcaa81fac0474f609131a9
kiettran1997 <70044100+kiettran1997@users.noreply.github.com>
2021-09-21 19:50:42 +0700

    update new lab2

--- Lab1/Chuong3/TopThreeAvg.py
+++ Lab1/Chuong3/TopThreeAvg.py
@@ -3,4 +3,8 @@ def top_three_avg(grade1, grade2, grade3, grade4):
  total = grade1 + grade2 + grade3 + grade4
  top_three = total - min(grade1, grade2, grade3, grade4)
  return top_three / 3
+
+ # hoặc
+ #return max(average_grade(grade1, grade2, grade3)
+ #return (grade1 + grade2 + grade3) / 3
  
